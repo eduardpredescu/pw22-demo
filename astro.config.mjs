a2adec24058eb34b4f78a5dd8119a3825b3a8dbe
Eduard Florin Predescu <eduard.predescu@softvision.com>
2022-09-12 22:53:49 +0300

    remove mdx

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,8 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import vue from "@astrojs/vue";
 import tailwind from "@astrojs/tailwind";
 
-import mdx from "@astrojs/mdx";
-
 // https://astro.build/config
 export default defineConfig({
-  integrations: [vue(), tailwind(), mdx()]
+  integrations: [vue(), tailwind()],
 });
